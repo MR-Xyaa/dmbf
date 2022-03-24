f2bb2ae57bd1f66d6c690b9a805ebe6508666c9a
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-25 06:09:37 +0700

    Update dmbf.py

--- dmbf.py
+++ dmbf.py
@@ -45,7 +45,7 @@ _logo_line_1_ = ('%s ___  __  __ ___ ___ '%(_P_))
 _logo_line_2_ = ('%s|   \|  \/  | _ ) __| %s┌─────────────────────────┐'%(_P_,_U_))
 _logo_line_3_ = ('%s| |) | |\/| | _ \ _|  %s│   %s• Code By MR-Xyaa •   %s│'%(_P_,_U_,_P_,_U_))
 _logo_line_4_ = ('%s|___/|_|  |_|___/_|   %s│ %sGithub.com/MR-Xyaa %s│'%(_P_,_U_,_P_,_U_))
-_logo_line_5_ = ('%s XNSCODE Team 2021    └─────────────────────────┘'%(_U_))
+_logo_line_5_ = ('%s MR-Xyaa CODE 2022    └─────────────────────────┘'%(_U_))
 def _my_logo_():
     _dapunta_cici_(_logo_line_1_)
     _dapunta_cici_(_logo_line_2_)
